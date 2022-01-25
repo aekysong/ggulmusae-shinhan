f3b0a985c35cc1160a4ed7314af7e3249702263b
ohee92 <ohee92@naver.com>
2022-01-25 10:03:30 +0900

    -팝업 그림 2개 랜덤하게 -버튼 추가

--- app/src/main/java/com/example/ggulmusae_shinhan/PopupActivity.java
+++ app/src/main/java/com/example/ggulmusae_shinhan/PopupActivity.java
@@ -5,8 +5,13 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class PopupActivity extends Activity {
+
+    ImageView imageview_celebrity;
+    TextView textview_celebrity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +19,25 @@ public class PopupActivity extends Activity {
         //타이틀바 없애기
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.popup);
+
+        imageview_celebrity = findViewById(R.id.celebrity_image);
+        textview_celebrity = findViewById(R.id.celebrity_name);
+
+        int random = (int)((Math.random()*10000)%10) %2;
+
+        switch(random){
+            case 0:
+                imageview_celebrity.setImageResource(R.drawable.warren1);
+                textview_celebrity.setText("워렌 버핏");
+                break;
+            case 1:
+                imageview_celebrity.setImageResource(R.drawable.charles);
+                textview_celebrity.setText("찰스 멍거");
+//            case 2:
+//                imageview_celebrity.setImageResource(R.drawable.peter);
+//                textview_celebrity.setText("피터 린치");
+        }
+
 
     }
 
@@ -25,6 +49,9 @@ public class PopupActivity extends Activity {
                 this.finish();
                 break;
             case R.id.share:
+                this.finish();
+                break;
+            case R.id.go_invest_plus:
                 this.finish();
                 break;
         }
